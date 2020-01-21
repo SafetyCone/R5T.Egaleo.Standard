6b5e7e4507a3d99fb61e5c517db67fc1ae250c1f
MinexAutomation <minex.automation@gmail.com>
2020-01-21 13:53:36 -0800

    Added IServiceCollection extensions.

---
+++ source/R5T.Egaleo.Standard/Code/Services/Extensions/IServiceCollectionExtensions.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+using Microsoft.Extensions.DependencyInjection;
+
+using R5T.Egaleo.Default;
+
+
+namespace R5T.Egaleo.Standard
+{
+    public static class IServiceCollectionExtensions
+    {
+        public static IServiceCollection AddOrganizationDirectoryNameConvention(this IServiceCollection services)
+        {
+            services.AddSingleton<IOrganizationDirectoryNameConvention, OrganizationDirectoryNameConvention>();
+
+            return services;
+        }
+    }
+}
